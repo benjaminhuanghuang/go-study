5a5a449127d0fceaf18fec5f340b1f31c2ef58c3
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-19 15:24:41 -0700

    update

---
+++ command-line.go
@@ -0,0 +1,28 @@
+/*
+	go run command-line.go -port=5151 -path=site
+
+	go run command-line.go -help
+	
+*/
+package main
+
+import (
+	"flag"
+	"net/http"
+	 "os"
+	)
+
+func main(){
+	var dir string
+
+	port := flag.String("port", "3000", "port to serve HTTP on")
+	path := flag.String("path", "", "path to server")
+	flag.Parse()
+
+	if *path ==""{
+		dir, _ = os.Getwd()
+	}else{
+		dir = *path
+	}
+	http.ListenAndServe(":"+*port, http.FileServer(http.Dir(dir)))
+}
--- file-server.go
+++ file-server.go
@@ -6,6 +6,7 @@ import (
 	)
 
 func main(){
+	// return dir and error
 	dir, _ := os.Getwd();
 	http.ListenAndServe(":3000", http.FileServer(http.Dir(dir)))
 }
