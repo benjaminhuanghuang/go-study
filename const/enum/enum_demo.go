fb8329bc40346fffea2ae725f8ae8efcac7f9c02
Benjamin Huang <benjaminhuanghuang@gmail.com>
2019-09-05 12:54:14 -0700

    update

--- const/enum/enum_demo.go
+++ const/enum/enum_demo.go
@@ -1,17 +1,22 @@
+/*
+enum 是一个const block
+
+enum 可以用iota自增
+
+可以用_skip某个值
+
+
+Reference
+	https://blog.learngoprogramming.com/golang-const-type-enums-iota-bc4befd096d3
+*/
 package main
 
-func enums() {
-	const (
-		cpp = 0
-		java
-		python
-		golang
-	)
-}
+// Declare a new custom type: Weekday
+type Weekday int
 
 //...
 const (
-	Monday = iota + 1
+	Monday Weekday = iota + 1
 	Tuesday
 	Wednesday
 	Thursday
@@ -19,6 +24,55 @@ const (
 	Saturday
 	Sunday
 )
+
+// Timezone enum
+type Timezone int
+
+const (
+	// iota: 0, EST: -5
+	EST Timezone = -(5 + iota)
+	// iota: 1, CST: -6
+	CST
+	// iota: 2, MST: -7
+	MST
+	// iota: 3, MST: -8
+	PST
+)
+
+/***********************************************
+Bitwise operations
+***********************************************/
+type Month int
+
+const (
+	January  Month = 1 << iota // 1 << 0 ==> 1
+	February                   // 1 << 1 ==> 2
+	March                      // 1 << 2 ==> 4
+	April                      // 1 << 3 ==> 8
+	May                        // 1 << 4 ==> 16
+	June                       // ...
+	July
+	August
+	September
+	October
+	November
+	December
+	AllMonths = January | February |
+		March | April | May | June |
+		July | August | September |
+		October | November |
+		December
+)
+
+func enums() {
+	const (
+		cpp = 0
+		_
+		java
+		python
+		golang
+	)
+}
 
 /*
 	Using iota
@@ -43,6 +97,6 @@ const (
 
 func main() {
 	println(Open)
-	println(b)
-	println(kb)
+	println(b, kb, mb, gb, tb, pb)
+	println(AllMonths)
 }
